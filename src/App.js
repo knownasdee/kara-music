7f5bc54803a95ea6662ec5afb45b919e1e03701d
Dada Delimustafic <dadadelimustafic@gmail.com>
2020-05-05 17:12:21 +0200

    displays youtube video on homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import ReactPlayer from 'react-player';
 
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-        <div class="title">
+        <div className="title">
             <b><span>K</span>A<span>R</span>I<span>NH</span>O</b>
+        </div>
+        <div className="content">
+            <div className="video">
+                <ReactPlayer width="400px" height="250px" url="https://www.youtube.com/watch?v=-KkJEZ5Atgw" />
+            </div>
         </div>
     </div>
   );
